4c86a2b3668c44d9211e3c0d63dd9ab18a4a8b49
Андрей <@192.168.100.5>
2017-05-08 12:01:51 +0300

    constructor

---
+++ tutor24/src/tutor24/Birds.java
@@ -0,0 +1,26 @@
+package tutor24;
+
+public class Birds {
+private String name;
+private boolean canFly;
+
+public Birds(String name,boolean canFly){
+	this.name=name;
+	this.canFly=canFly;
+	
+}
+public String getName() {
+	return name;
+}
+
+
+public boolean isCanFly() {
+	return canFly;
+}
+
+}
+
+
+
+
+
---
+++ tutor24/src/tutor24/Main.java
@@ -0,0 +1,14 @@
+package tutor24;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Birds bird=new Birds("Синица ",true);
+		
+		
+		System.out.println(bird.getName());
+		System.out.println(bird.isCanFly());
+		
+	}
+
+}
